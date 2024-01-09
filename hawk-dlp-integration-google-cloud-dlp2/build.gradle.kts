80ed9fed6987d26781564b510f99db479c335cf7
Paskal Paesler <paskalpaesler23@gmail.com>
2024-01-09 17:09:32 +0100

    Update to Spring Boot 3.2

--- hawk-dlp-integration-google-cloud-dlp2/build.gradle.kts
+++ hawk-dlp-integration-google-cloud-dlp2/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.0"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm")
-    kotlin("plugin.spring") version "1.7.20"
+    kotlin("plugin.spring") version "1.9.21"
 }
 
 group = "io.hawk.dlp"
